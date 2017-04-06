68164886713b65d81e291e0c7cc1a9ab93f9b3bd
Adam Ahrens <adam.aahrens@gmail.com>
2017-04-06 15:05:39 -0500

    Initial Project Commit

---
+++ OpenSwift/OpenSwift/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  OpenSwift
+//
+//  Created by Adam Ahrens on 4/6/17.
+//  Copyright © 2017 Adam Ahrens. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+
+  }
+}
